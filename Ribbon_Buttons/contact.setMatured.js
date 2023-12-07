a4163aba6c8e528537c7ff9e0f8766bbfdef0e92
Bernard Olivier <88403974+Bazz94@users.noreply.github.com>
2023-12-07 14:22:42 +0200

    Finished setMatured ribbon button

--- Ribbon_Buttons/contact.setMatured.js
+++ Ribbon_Buttons/contact.setMatured.js
@@ -1,40 +1,85 @@
-var Contact = window.Contact || {};
+var Contact2 = window.Contact2 || {};
 (function () {
+
+  const notification1 = this.generateUniqueNumber;
+  const notification2 = this.generateUniqueNumber;
+  const notification3 = this.generateUniqueNumber;
   const statusCodes = {
-    ACTIVE: "0",
-    INACTIVE: "1",
+    ACTIVE: 0,
+    INACTIVE: 1,
   };
 
   const statusReason = {
-    MATURED: "2",
+    MATURED: 2,
   };
 
   const logicalNames = {
     STATUS: "statecode",
-    STATUS_REASON: "statusreason",
+    STATUS_REASON: "statuscode",
     JOIN_DATE: "ss_joiningdate",
     MATURED_DATE: "ss_maturitydate",
   };
 
-  this.setMatured = (formContext) => {
-    console.log("test");
+  this.setMaturedButton = (formContext) => {
     // get values
+    const today = new Date();
     const statusCodeAttribute = formContext.getAttribute(logicalNames.STATUS);
     const statusCodeValue = statusCodeAttribute.getValue();
     const statusReasonAttribute = formContext.getAttribute(logicalNames.STATUS_REASON);
-    const statusReasonValue = statusCodeAttribute.getValue();
-    const joinDateAttribute = formContext.getAttribute(logicalNames.JOIN_DATE);
-    const joinDateValue = statusCodeAttribute.getValue();
     const maturityDateAttribute = formContext.getAttribute(logicalNames.MATURED_DATE);
-    const maturityDateValue = statusCodeAttribute.getValue();
-    console.log("Join date: ", joinDateValue);
+    //// temp value until plugins have been made
+    maturityDateAttribute.setValue(today);
+    ////
+    const maturityDateValue = new Date(maturityDateAttribute.getValue());
+
     // check if investment has matured and if status is active
-    if (true) {
-      // set status and status reason
-      statusCodeAttribute.setValue(statusCodes.INACTIVE);
-      statusReasonAttribute.setValue(statusReason.MATURED);
+    if (today >= maturityDateValue) {
+      if (statusCodeValue === statusCodes.ACTIVE) {
+        // set status and status reason
+        statusCodeAttribute.setValue(statusCodes.INACTIVE);
+        statusReasonAttribute.setValue(statusReason.MATURED);
+        formContext.data.refresh(true);
+      } else {
+        formContext.ui.setFormNotification("This investment is inactive", "WARNING", notification1);
+        // Wait for 5 seconds before clearing the notification
+        window.setTimeout(function () {
+          formContext.ui.clearFormNotification(notification1);
+        }, 10 * 1000);
+      }
+    } else {
+      formContext.ui.setFormNotification("This investment has not matured yet", "INFO", notification2);
+      // Wait for 5 seconds before clearing the notification
+      window.setTimeout(function () {
+        formContext.ui.clearFormNotification(notification2);
+      }, 10 * 1000);
     }
-    Xrm.Page.data.save();
-    Xrm.Page.data.refresh();
   };
-}).call(Contact);
+
+  this.enabledRule = (formContext) => {
+    const today = new Date();
+    const maturityDateAttribute = formContext.getAttribute(logicalNames.MATURED_DATE);
+    //// temp value until plugins have been made
+    maturityDateAttribute.setValue(today);
+    ////
+    const maturityDateValue = new Date(maturityDateAttribute.getValue());
+    // Set the button's visibility based on the condition
+    const statusCodeAttribute = formContext.getAttribute(logicalNames.STATUS);
+    const statusCodeValue = statusCodeAttribute.getValue();
+    const setVisible = today >= maturityDateValue && statusCodeValue === statusCodes.ACTIVE;
+    if (setVisible) {
+      formContext.ui.setFormNotification("This investment can be matured", "INFO", notification3);
+      // Wait for 5 seconds before clearing the notification
+      window.setTimeout(function () {
+        formContext.ui.clearFormNotification(notification3);
+      }, 10 * 1000);
+    }
+    return setVisible;
+  };
+
+  this.generateUniqueNumber = () => {
+    var timestamp = new Date().getTime();
+    var randomNumber = Math.floor(Math.random() * 1000) + 1;
+    var uniqueNumber = timestamp + randomNumber;
+    return uniqueNumber;
+}
+}).call(Contact2);
